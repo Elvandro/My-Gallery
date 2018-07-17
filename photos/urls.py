e0fb66f6a256f719180099ce61cf68b4d5a935df
Elvandro <elvisamuni22@gmail.com>
2018-07-17 23:37:13 +0300

    Commit to create url patterns

--- photos/urls.py
+++ photos/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
     url(r'^$', views.home, name = 'home'),
+    url(r'search',views.search_results, name ='search_results'),
+    url(r'image/<int:image_id>',views.image, name='one_image'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
